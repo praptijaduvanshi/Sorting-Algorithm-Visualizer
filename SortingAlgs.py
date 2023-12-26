2b10264488b70a22b2a8a170a4e20551077b2282
Prapti Jaduvanshi <khushi.jaduvanshi@gmail.com>
2023-12-26 16:57:22 +0700

    Generating  starting list

--- SortingAlgs.py
+++ SortingAlgs.py
@@ -15,6 +15,11 @@ class gameInformation:
     BLUE= 0, 0, 255
     BACKGROUND_COLOR= WHITE
 
+    SIDE_PAD=100
+    TOP_PAD= 150
+
+
+
 def __init__(self, width, height, list):
     self.width= width
     self.height= height
@@ -25,4 +30,20 @@ def __init__(self, width, height, list):
 
 
 def set_list(self,list):
-    pass
+    self.list= list
+    self.minimum_val= min(list)
+    self.maximum_val= max(list)
+
+    self.block_width= round((self.width - self.SIDE_PAD) / len(list))
+    self.block_heigh= round((self.height - self.TOP_PAD) / (self.maximum_val - self.minimum_val))
+    self.start_x= self.SIDE_PAD // 2
+
+#Generating starting list
+def generate_start_list(n, minimum_val, maximum_val):
+    list = []
+
+    for _ in range(n):
+        value = random.randint(minimum_val, maximum_val)
+        list.append(value)
+    
+    return list

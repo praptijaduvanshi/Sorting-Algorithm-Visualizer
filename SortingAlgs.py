85bd2699c0374019ef676c9870d08f3e5de69f8d
Prapti Jaduvanshi <khushi.jaduvanshi@gmail.com>
2023-12-26 17:57:45 +0700

    Completed setting up main

--- SortingAlgs.py
+++ SortingAlgs.py
@@ -18,25 +18,22 @@ class gameInformation:
     SIDE_PAD=100
     TOP_PAD= 150
 
+    def __init__(self, width, height, list):
+        self.width= width
+        self.height= height
+        
+        self.window= pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Sorting Algorithm Visualizer")
+        self.set_list(list)
 
+    def set_list(self,list):
+        self.list= list
+        self.minimum_val= min(list)
+        self.maximum_val= max(list)
 
-def __init__(self, width, height, list):
-    self.width= width
-    self.height= height
-    
-    self.window= pygame.display.set_mode((width, height))
-    pygame.display.set_caption("Sorting Algorithm Visualizer")
-    self.list(list)
-
-
-def set_list(self,list):
-    self.list= list
-    self.minimum_val= min(list)
-    self.maximum_val= max(list)
-
-    self.block_width= round((self.width - self.SIDE_PAD) / len(list))
-    self.block_heigh= round((self.height - self.TOP_PAD) / (self.maximum_val - self.minimum_val))
-    self.start_x= self.SIDE_PAD // 2
+        self.block_width= round((self.width - self.SIDE_PAD) / len(list))
+        self.block_height= round((self.height - self.TOP_PAD) / (self.maximum_val - self.minimum_val))
+        self.start_x= self.SIDE_PAD // 2
 
 #Generating starting list
 def generate_start_list(n, minimum_val, maximum_val):
@@ -47,3 +44,30 @@ def generate_start_list(n, minimum_val, maximum_val):
         list.append(value)
     
     return list
+
+#Render the screen, set up main event loop
+def main():
+    run = True
+    clock=  pygame.time.Clock()
+
+    n= 50
+    minimum_val= 0
+    maximum_val= 100
+
+    list= generate_start_list(n,minimum_val,maximum_val)
+    draw_info= gameInformation(800, 600, list)
+
+    while run:
+        clock.tick(60)
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame. QUIT:
+                run = False
+
+    pygame.quit
+
+if  __name__== "__main__":
+    main()
+
+
